7c01fe29c5b60e700d45181832f1276689b0e821
hammad ul haq <hammadulhaqlodhi@gmail.com>
2026-04-02 13:50:30 +0500

    feat: implement gold standard SEO (metadata, sitemap, robots)

--- models/signup.ts
+++ models/signup.ts
@@ -3,6 +3,7 @@ import mongoose, { Schema, Document, models, model} from "mongoose";
 export interface IUser extends Document {
   username: string;
   password: string;
+  role: "admin" | "user";
   createdAt: Date;
   updatedAt: Date;
 }
